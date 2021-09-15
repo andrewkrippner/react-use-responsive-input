10e1a5d3f3f5d0e3fc9428022a432f794ca426d1
Andrew Krippner <andrewkrip@gmail.com>
2021-09-15 15:04:10 -0700

    put calculator divs in singleton container div with hidden overflow to prevent taking up height in the body

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,24 @@ const useResponsiveInput = (options?: Partial<UseResponsiveInputOptions>) => {
 
     useEffect(() => {
         if (!ref.current) return
+        const id = 'useResponsiveInputContainer'
+        const createSingletonDiv = () => {
+            const singletonDiv = document.createElement('div')
+            singletonDiv.id = id
+            singletonDiv.style.height = '0px'
+            singletonDiv.style.overflow = 'hidden'
+            document.body.appendChild(singletonDiv)
+            return singletonDiv
+        }
+        const singletonDiv = document.getElementById(id) || createSingletonDiv()
         const computedStyle = window.getComputedStyle(ref.current)
         const div = document.createElement('div')
-        div.id = 'useResponsiveInputReference'
         div.style.padding = computedStyle.padding
         div.style.font = computedStyle.font
         div.style.visibility = 'hidden'
         div.style.display = 'inline'
         div.innerHTML = ref.current.value.split(' ').join('&nbsp;') || ''
-        document.body.appendChild(div)
+        singletonDiv.appendChild(div)
         ref.current.style.width = `${div.offsetWidth}px`
         return () => div.remove()
     })
