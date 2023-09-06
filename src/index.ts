5175b8770b290d078b76192d9a355b312b381e21
Andrew Krippner <andrewkrip@gmail.com>
2023-09-06 13:27:57 -0700

    add workflows, format, update useeffect behavior, add renovate, add vite, update tsconfig and gitignore

--- src/index.ts
+++ src/index.ts
@@ -7,46 +7,52 @@ interface UseResponsiveInputOptions {
 }
 
 const useResponsiveInput = (
-    options: Partial<UseResponsiveInputOptions> = {}
+    options: Partial<UseResponsiveInputOptions> = {},
 ) => {
     const { minWidth = 0, extraWidth = 0, disabled = false } = options
 
     const ref = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
-        if (!ref.current) return
-        const id = 'useResponsiveInputContainer'
-        const createSingletonDiv = () => {
-            const singletonDiv = document.createElement('div')
-            singletonDiv.id = id
-            singletonDiv.style.height = '0px'
-            singletonDiv.style.overflow = 'hidden'
-            document.body.appendChild(singletonDiv)
-            return singletonDiv
+        const updateWidth = () => {
+            if (!ref.current) return
+            console.log(`Updating Width of ${ref.current.id}`)
+            const id = 'useResponsiveInputContainer'
+            const createSingletonDiv = () => {
+                const singletonDiv = document.createElement('div')
+                singletonDiv.id = id
+                singletonDiv.style.height = '0px'
+                singletonDiv.style.overflow = 'hidden'
+                document.body.appendChild(singletonDiv)
+                return singletonDiv
+            }
+            const singletonDiv =
+                document.getElementById(id) ?? createSingletonDiv()
+            const computedStyle = window.getComputedStyle(ref.current)
+            const div = document.createElement('div')
+            div.style.padding = computedStyle.padding
+            div.style.font = computedStyle.font
+            div.style.border = computedStyle.border
+            div.style.margin = computedStyle.margin
+            div.style.visibility = 'hidden'
+            div.style.display = 'inline'
+            div.style.height = '0px'
+            div.style.overflow = 'hidden'
+            div.style.whiteSpace = 'pre'
+            div.innerHTML = ref.current.value.split(' ').join('&nbsp') || ''
+            singletonDiv.appendChild(div)
+            if (!disabled) {
+                ref.current.style.width = `${Math.max(
+                    div.offsetWidth + extraWidth,
+                    minWidth,
+                )}px`
+                ref.current.style.boxSizing = 'border-box'
+            }
+            div.remove()
         }
-        const singletonDiv = document.getElementById(id) ?? createSingletonDiv()
-        const computedStyle = window.getComputedStyle(ref.current)
-        const div = document.createElement('div')
-        div.style.padding = computedStyle.padding
-        div.style.font = computedStyle.font
-        div.style.border = computedStyle.border
-        div.style.margin = computedStyle.margin
-        div.style.visibility = 'hidden'
-        div.style.display = 'inline'
-        div.style.height = '0px'
-        div.style.overflow = 'hidden'
-        div.style.whiteSpace = 'pre'
-        div.innerHTML = ref.current.value.split(' ').join('&nbsp') || ''
-        singletonDiv.appendChild(div)
-        if (!disabled) {
-            ref.current.style.width = `${Math.max(
-                div.offsetWidth + extraWidth,
-                minWidth
-            )}px`
-            ref.current.style.boxSizing = 'border-box'
-        }
-        return () => div.remove()
-    }, [ref.current?.value])
+        ref.current?.addEventListener('change', updateWidth)
+        return () => ref.current?.removeEventListener('change', updateWidth)
+    }, [])
 
     return ref
 }
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import { resolve } from 'path'
+import { UserConfig, defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import react from '@vitejs/plugin-react'
+import { name } from './package.json'
+
+export default defineConfig(() => {
+    const config: UserConfig = {
+        build: {
+            lib: {
+                name: 'React Use Responsive Input',
+                entry: resolve(__dirname, 'src/index.ts'),
+                formats: ['es', 'umd'],
+                fileName: (format) => `${name}.${format}.js`,
+            },
+            rollupOptions: {
+                external: ['react', 'react/jsx-runtime', 'react-dom'],
+                output: {
+                    globals: {
+                        react: 'React',
+                        'react/jsx-runtime': 'react/jsx-runtime',
+                        'react-dom': 'ReactDOM',
+                    },
+                },
+            },
+            commonjsOptions: {
+                include: [/node_modules/],
+            },
+            outDir: 'dist',
+            sourcemap: false,
+        },
+        plugins: [
+            react(),
+            dts({
+                insertTypesEntry: true,
+            }),
+        ],
+    }
+    return config
+})
